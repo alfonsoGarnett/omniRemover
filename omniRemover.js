f4854c3b5e58a820dc91cbf6f7b03663d2a2e9a4
alfonsoGarnett <lentiformnucleus@gmail.com>
2015-01-16 15:12:13 -0600

    UI changes, switch statements

--- omniRemover.js
+++ omniRemover.js
@@ -7,7 +7,7 @@
     var body = document.getElementsByTagName("body")[0];
     var clickRemovesFlag = true; //Default for click element removal
     var preventLinksFlag = true; //Default for preventing URL action on link click
-    var minimized = false;       //Initialize minimized
+    var minimized = false; //Initialize minimized
     var enableButtonsFlag = false; //Buttons will be 'enabled' first, on menu option click
 
     // ##########################
@@ -15,6 +15,7 @@
     // ##########################
 
     var style = document.createElement('style');
+
     function styleSheet(set)
     {
         if (set === true)
@@ -26,26 +27,27 @@
             // # ID floatingWindow (main window) #
             // ###################################
 
-            style.innerHTML = '#floatingWindow { z-index: 9445656445656; line-height: 1.2; position: fixed; overflow: hidden; top: 10%; width: 14%; height: 40%;' + 
-                              'background: #000; color: #fff; opacity: 0.8; border: 1px solid lightblue; padding: 1%; font-size: 0.75vmax; }';
+            style.innerHTML = '#floatingWindow { z-index: 9445656445656; line-height: 1.9vh; position: fixed; overflow: hidden; top: 10%; width: 29vh; height: 31vh;' +
+                'background: rgba(0, 0, 0, 0.9); color: #efefef; padding: 1%; border-radius: 10px; border: 1px solid transparent; box-shadow: 0 0 5px rgba(46, 141, 216, 0.5);' +
+                 '}';
             // ##########################################################################
             // # ID floatingWindowData (main window's display. Displays element's data) #
             // ##########################################################################
 
-            style.innerHTML += '#floatingWindowData { overflow: hidden; max-height: 79%; min-height: 40px; }';
+            style.innerHTML += '#floatingWindowData { overflow: hidden; height: 67%; min-height: 60px; font-size: 1.75vh; border-bottom: 1px solid rgba(46, 141, 216, 0.5);}';
 
             // #######################################################
             // # ID floatingWindowOptions (main window options' div) #
             // #######################################################
 
-            style.innerHTML += '#floatingWindowOptions { z-index: 9445656445657; position: absolute; bottom: 20%; width: 100%; height: 5%; background: transparent;' +
-            'color: lightblue; opacity: 0.8; padding: 1%; font-size: 0.65vmax; }';
+            style.innerHTML += '#floatingWindowOptions { z-index: 9445656445657; position: absolute; bottom: 8.8vh; width: 100%; height: 5%; background: transparent;' +
+                'color: lightblue; opacity: 0.8; padding: 1%; font-size: 1.5vh; }';
 
             // #################################################################################
             // # CLASS fWOption (for menu elements, or elements that must be made 'clickable') #
             // #################################################################################
 
-            style.innerHTML += '.fWOption { color: lightblue; cursor: pointer }';
+            style.innerHTML += '.fWOption { color: #2E8DD8; cursor: pointer }';
 
             // # Append style to document
             document.getElementsByTagName('head')[0].appendChild(style);
@@ -118,116 +120,123 @@
     var mouseoverEventListener = function(e)
     {
         floatingWindowData.innerHTML =
-            "<p><b>Type: </b>" + e.target.tagName + "</p>" + "<p><b>ID: </b>" + e.target.getAttribute("id") + "</p>" + "<p><b>Class: </b>" + e.target.getAttribute("class") + "</p>" + "<p><b>Target: </b>" + e.target + "</p>";
+            "<p><b>Type: </b>" + e.target.tagName + "</p>" + 
+            "<p><b>ID: </b>" + e.target.getAttribute("id") + "</p>" + 
+            "<p><b>Class: </b>" + e.target.getAttribute("class") + "</p>" + 
+            "<p><b>Target: </b>" + e.target + "</p>";
     };
     document.addEventListener("mouseover", mouseoverEventListener);
 
     var documentClickListener = function(e)
     {
-        if (e.target.getAttribute("id") == "clickRemoves")
+        switch (e.target.getAttribute("id"))
         {
-            switch (clickRemovesFlag)
-            {
-                case true:
-                    console.log("Element removal by click disabled");
-                    e.target.innerHTML = "Click removes element: NO";
-                    break;
 
-                case false:
-                    console.log("Element removal by click enabled");
-                    e.target.innerHTML = "Click removes element: YES";
-                    break;
-            }
-            clickRemovesFlag = !clickRemovesFlag;
-            return false;
+            case "clickRemoves":
+
+                switch (clickRemovesFlag)
+                {
+                    case true:
+                        console.log("Element removal by click disabled");
+                        e.target.innerHTML = "Click removes element: NO";
+                        break;
+
+                    case false:
+                        console.log("Element removal by click enabled");
+                        e.target.innerHTML = "Click removes element: YES";
+                        break;
+                }
+                clickRemovesFlag = !clickRemovesFlag;
+                return false;
+
+            case "preventLinks":
+
+                switch (preventLinksFlag)
+                {
+                    case true:
+                        e.target.innerHTML = "Prevent links: NO";
+                        preventLinks(false);
+                        break;
+
+                    case false:
+                        e.target.innerHTML = "Prevent links: YES";
+                        preventLinks(true);
+                        break;
+                }
+                preventLinksFlag = !preventLinksFlag;
+                return false;
+
+            case "minimizeDivRem":
+
+                switch (minimized)
+                {
+                    case false:
+                        document.removeEventListener("mouseover", mouseoverEventListener);
+                        floatingWindow.style.height = "1%";
+                        floatingWindowOptions.style.display = "none";
+                        floatingWindowData.innerHTML = "<span id='minimizeDivRem' class='fWOption'>Maximize</span>";
+                        console.log("Window minimized");
+                        break;
+
+                    case true:
+                        document.addEventListener("mouseover", mouseoverEventListener);
+                        floatingWindowData.innerHTML = "";
+                        floatingWindow.style.height = "38%";
+                        floatingWindowOptions.style.display = "block";
+                        console.log("Window maximized");
+                        break;
+
+                }
+                minimized = !minimized;
+                return false;
+
+            case "enableButtons":
+
+                var enableButtonsButton = document.getElementsByTagName("button");
+                var enableButtonsInput = document.getElementsByTagName("input");
+                for (var i = 0; i < enableButtonsButton.length; i++)
+                {
+                    enableButtonsButton[i].disabled = enableButtonsFlag;
+                    console.log("<button>: ", enableButtonsButton[i]);
+                }
+                for (var i = 0; i < enableButtonsInput.length; i++)
+                {
+                    enableButtonsInput[i].disabled = enableButtonsFlag;
+                    console.log("<input>: ", enableButtonsInput[i]);
+                }
+                enableButtonsFlag = !enableButtonsFlag;
+                return false;
+
+            case "displayHidden":
+
+                var documentElems = document.getElementsByTagName("div");
+                for (var i = 0; i < documentElems.length; i++)
+                {
+                    documentElems[i].style.display = "block";
+                    console.log(i, " hidden elements are now visible.");
+                }
+                return false;
+
+            case "closeDivRem":
+
+                styleSheet("destroy");
+                floatingWindow.parentNode.removeChild(floatingWindow);
+                preventLinksFlag = false;
+                preventLinks(false);
+                clickRemovesFlag = false;
+                document.removeEventListener("mouseover", mouseoverEventListener);
+                document.removeEventListener("click", documentClickListener);
+                console.log("Element removal by click disabled");
+                console.log("#############################################################");
+                console.log("Script stopped, everything back to normal. Please reload page");
+                return false;
         }
 
-        if (e.target.getAttribute("id") == "preventLinks")
-        {
-            switch (preventLinksFlag)
-            {
-                case true:
-                    e.target.innerHTML = "Prevent links: NO";
-                    preventLinks(false);
-                    break;
-
-                case false:
-                    e.target.innerHTML = "Prevent links: YES";
-                    preventLinks(true);
-                    break;
-            }
-            preventLinksFlag = !preventLinksFlag;
-            return false;
-        }
-        if (e.target.getAttribute("id") == "minimizeDivRem")
-        {
-            switch (minimized)
-            {
-                case false:
-                    document.removeEventListener("mouseover", mouseoverEventListener);
-                    floatingWindow.style.height = "1%";
-                    floatingWindowOptions.style.display = "none";
-                    floatingWindowData.innerHTML = "<span id='minimizeDivRem' class='fWOption'>Maximize</span>";
-                    console.log("Window minimized");
-                    break;
-
-                case true:
-                    document.addEventListener("mouseover", mouseoverEventListener);
-                    floatingWindowData.innerHTML = "";
-                    floatingWindow.style.height = "38%";
-                    floatingWindowOptions.style.display = "block";
-                    console.log("Window maximized");
-                    break;
-
-            }
-            minimized = !minimized;
-            return false;
-        }
-        if (e.target.getAttribute("id") == "enableButtons")
-        {
-            var enableButtonsButton = document.getElementsByTagName("button");
-            var enableButtonsInput = document.getElementsByTagName("input");
-            for (var i = 0; i < enableButtonsButton.length; i++)
-            {
-                enableButtonsButton[i].disabled = enableButtonsFlag;
-                console.log("<button>: ", enableButtonsButton[i]);
-            }
-            for (var i = 0; i < enableButtonsInput.length; i++)
-            {
-                enableButtonsInput[i].disabled = enableButtonsFlag;
-                console.log("<input>: ", enableButtonsInput[i]);
-            }
-            enableButtonsFlag = !enableButtonsFlag;
-            return false;
-        }
-        if (e.target.getAttribute("id") == "displayHidden")
-        {
-            var documentElems = document.getElementsByTagName("div");
-            for (var i = 0; i < documentElems.length; i++)
-            {
-                documentElems[i].style.display = "block";
-                console.log(i, " hidden elements are now visible.");
-            }
-            return false;
-        }
-        if (e.target.getAttribute("id") == "closeDivRem")
-        {
-            styleSheet("destroy");
-            floatingWindow.parentNode.removeChild(floatingWindow);
-            preventLinksFlag = false;
-            preventLinks(false);
-            clickRemovesFlag = false;
-            document.removeEventListener("mouseover", mouseoverEventListener);
-            document.removeEventListener("click", documentClickListener);
-            console.log("Element removal by click disabled");
-            console.log("#############################################################");
-            console.log("Script stopped, everything back to normal. Please reload page");
-            return false;
-        }
         if (floatingWindow.contains(e.target))
         {
             return false;
         }
+
         if (clickRemovesFlag == true)
         {
             var clickedThis = e.target;
